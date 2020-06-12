b66b5b6c13c85424184a65fb114e10f53beb915d
atembamanu <atembaemm07@gmail.com>
2020-06-12 17:00:35 +0300

    Donuts

---
+++ scripts.js
@@ -0,0 +1,75 @@
+var circle = new circleDonutChart("don1");
+circle.draw({
+  end: 75,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "green",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+
+var circle = new circleDonutChart("proj1");
+circle.draw({
+  end: 98,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "green",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+
+var circle = new circleDonutChart("proj2");
+circle.draw({
+  end: 98,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "green",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+
+var circle = new circleDonutChart("proj3");
+circle.draw({
+  end: 12,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "red",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+
+var circle = new circleDonutChart("proj4");
+circle.draw({
+  end: 40,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "red",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+
+var circle = new circleDonutChart("proj5");
+circle.draw({
+  end: 50,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "red",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
+var circle = new circleDonutChart("proj6");
+circle.draw({
+  end: 76,
+  start: 0,
+  maxValue: 100,
+  outerCircleColor: "#80ABD9",
+  innerCircleColor: "grey",
+  size: 45,
+  angleOfStart: -180,
+});
